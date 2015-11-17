04847720f615a2f883a91761988ae42007b2e5d3
Gazolik <Gazolike@gmail.com>
2015-11-17 16:01:06 +0100

    module2 call

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import subprocess
-# from Module import module2_partie1
+from Module import module2_partie1
 from flask import json
+
 
 def DoSearch(search):
     # call module 1 REQUEST TO URL TO TEXT URL
     print('bash call')
-    # subprocess.check_call(['./Module/module1.sh', search])
+    subprocess.check_call(['./Module/module1.sh', search])
     print('bash end')
     f = open('Module/output/alchemy.json', 'r', encoding='utf-8')
     string = f.read()
@@ -13,7 +14,8 @@ def DoSearch(search):
     jsonlist = dict['resultats']
     # call module 2 TEXT URL TO URI TO RDF
     print('module 2 call')
-    # module2_partie1.getUrlFromText(f.read())
+    urllist = module2_partie1.getUrlsFromTexts(jsonlist)
     print('module 2 end')
+    print(urllist)
     return 'rien'
     # call module 3 RDF TO RESULTS
