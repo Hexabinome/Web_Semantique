48ab4eba6b131c351ad55ac33dffa66bfe52061c
David-Wobrock <david.wobrock@gmail.com>
2015-11-17 16:45:43 +0100

    Add error output and adapt sparql queries (optimization + list of lists management)

--- Module/module2_partie2.py
+++ Module/module2_partie2.py
@@ -16,27 +16,43 @@ def getSparqlFromUrl(url):
     rdfTripletList = jsonResponse['results']['bindings']
     return json.dumps({url: rdfTripletList})
 
-def getSparqlFromUrlThreaded(url, resultList, idx):
-    resultList[idx] = getSparqlFromUrl(url)
+def getSparqlFromUrlThreaded(url, resultDict):
+    resultDict[url] = getSparqlFromUrl(url)
 
 '''
-Launches a sparql query and returns a list of json objects.
+Parameter is a list of lists of urls. It 
+Launches a sparql query for each different url
+Returns a list (of pages) of lists of jsonDBPediaContent
 '''
-def getSparqlFromUrls(urlList):
-    nbUrl = len(urlList)
-    toReturn = [None] * nbUrl
-    threads = [None] * nbUrl
+def getSparqlFromUrls(listOfListsOfUrls):
+    # Copy urls in one set => unique elements
+    urlDict = {}
+    for page in listOfListsOfUrls:
+        for url in page:
+            urlDict[url] = None
 
+    threads = []
+    
     # Launch threads
-    for i in range(nbUrl):
-        threads[i] = threading.Thread(target=getSparqlFromUrlThreaded, args=(urlList[i], toReturn, i))
-        threads[i].start()
+    for url in urlDict.keys():
+        t = threading.Thread(target=getSparqlFromUrlThreaded, args=(url, urlDict))
+        t.start()
+        threads.append(t)
 
     # Wait for threads
     for thread in threads:
         thread.join()
-    return toReturn
+
+    # Create out list
+    out_list = listOfListsOfUrls[:] # Realizes copy of elements
+    for pageIdx in range(len(out_list)):
+        for urlIdx in range(len(out_list[pageIdx])):
+            url = out_list[pageIdx][urlIdx]
+            out_list[pageIdx][urlIdx] = urlDict[url]
+
+    return out_list
 
 # Example calls
 #res = getSparqlFromUrl('http://dbpedia.org/resource/Beer') 
-#results = getSparqlFromUrls(['http://dbpedia.org/resource/Beer', 'http://dbpedia.org/resource/Germany'])
+#results = getSparqlFromUrls([['http://dbpedia.org/resource/Beer', 'http://dbpedia.org/resource/Germany', 'http://dbpedia.org/resource/Europe'],
+#                            ['http://dbpedia.org/resource/France', 'http://dbpedia.org/resource/Baguette', 'http://dbpedia.org/resource/Europe']])
