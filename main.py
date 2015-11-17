ffc9fe44004744bc614dcd78daa4f0896ce6a22c
Gazolik <Gazolike@gmail.com>
2015-11-17 14:15:48 +0100

    add doSearch

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import subprocess
-from Module import module2_partie1
+#from Module import module2_partie1
 
 
 def DoSearch(search):
     # call module 1 REQUEST TO URL TO TEXT URL
     subprocess.check_call(['./Module/module1.sh', search])
     f = open('Module/output/alchemy.json')
-
+    return f.read()
     # call module 2 TEXT URL TO URI TO RDF
-    module2_partie1.getUrlFromText(f.read)
+    #module2_partie1.getUrlFromText(f.read)
     # module2_partie2
     # call module 3 RDF TO RESULTS
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-# import main
+import main
 
 app = Flask(__name__)
 
@@ -7,7 +7,8 @@ app = Flask(__name__)
 @app.route("/search/<search>")
 def search(search):
     print('you searched '+search)
-    # main.DoSearch(search)
+    search = main.DoSearch(search)
+    print(search)
     return render_template('search.html', search=search)
 
 if __name__ == "__main__":
