626fec8b4f16129437fd54a9f4a5c46ae512df80
Gazolik <Gazolike@gmail.com>
2015-11-17 09:49:18 +0100

    module call

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import subprocess
+from Module import module3
 
+
+# call module 1 REQUEST TO URL TO TEXT URL
 subprocess.check_call(['./Module/01-requete-vers-urls.sh'])
+f = open('Module/output/alchemy.json')
+
+# call module 2 TEXT URL TO URI TO RDF
+module3.function('Module/output/alchemy.json')
+
+# call module 3 RDF TO RESULTS
