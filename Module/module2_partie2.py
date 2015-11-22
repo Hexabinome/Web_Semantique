5320234f7d6ec1fcb4f1032d2394a00e5d143636
Djowood <jolan.cornevin@etu.univ-lyon1.fr>
2015-11-22 11:09:59 +0100

    ajout du test pour les acteurs

--- Module/module2_partie2.py
+++ Module/module2_partie2.py
@@ -3,20 +3,28 @@
 #           get_sparql_graph
 # ------------------------------------------
 from SPARQLWrapper import SPARQLWrapper, JSON
-import threading, json
+import threading, json, sys
 
 # TODO : Reflechir sur les requetes a effectuee
 '''
 requestType is a number to change the dbpedia query
 '''
-
-
 def getSparqlFromUrl(url, requestType):
     options = {0: subject,
                1: item,
                2: subjectAndItem
                }
     query = options[requestType](url)
+    return doQuery(url, query)
+
+def testIsTargetType(url, target):
+    options = {0: actor,
+               1: film
+            }
+    query = options[target](url)
+    return doQuery(url, query)[url]
+
+def doQuery(url, query):
     sparql = SPARQLWrapper("http://dbpedia.org/sparql")
     sparql.setQuery(query)
     sparql.setReturnFormat(JSON)
@@ -25,35 +33,39 @@ def getSparqlFromUrl(url, requestType):
     rdfTripletList = jsonResponse['results']['bindings']
     return {url: json.loads(json.dumps(rdfTripletList))}
 
-
 def subject(url):
-    return "SELECT * WHERE {{ <{0}> ?predicat ?valeur }}".format(url)
-
+    request = "SELECT * WHERE {{ <{0}> ?predicat ?valeur}}".format(url)
+    return request
 
 def item(url):
     return "SELECT * WHERE {{  ?subject ?predicat <{0}> }}".format(url)
 
-
 def subjectAndItem(url):
     return " SELECT * WHERE{{ {{ {0} }}UNION{{ {1} }} }}".format(item(url), subject(url))
 
+def actor(url):
+    return " SELECT ?acteur WHERE{{ {{ ?acteur a umbel-rc:Actor. FILTER(?acteur = <{0}>) }}UNION{{ ?film dbo:starring ?acteur. FILTER(?acteur = <{0}>) }} }}".format(url)
 
-def getSparqlFromUrlThreaded(urls, resultDict, requestType):
+def film(url):
+    return "SELECT * WHERE {{ ?a rdf:type ?????. FILTER(?a = <{0}>) } UNION {?film dbo:starring ?acteur. FILTER(?acteur = <{0}>)}}".format(url)
+
+
+def getSparqlFromUrlThreaded(urls, resultUrlDict, resultTargetDict, requestType, target):
     for url in urls :
-        resultDict[url] = getSparqlFromUrl(url, requestType)
-
+        resultUrlDict[url] = getSparqlFromUrl(url, requestType)
+        if testIsTargetType(url, target) != []:
+            resultTargetDict[url] = url
 
 '''
 Parameter is a list of lists of urls. It
 Launches a sparql query for each different url
 Returns a list (of pages) of lists of jsonDBPediaContent
 '''
-
-
-def getSparqlFromUrls(listOfListsOfUrls, requestType):
+def getSparqlFromUrls(listOfListsOfUrls, requestType, target):
     # Copy urls in one set => unique elements
     urlDict = {}
     urlTab = []
+    targetDict = {}
     for page in listOfListsOfUrls:
         if page:
             for url in page:
@@ -67,7 +79,7 @@ def getSparqlFromUrls(listOfListsOfUrls, requestType):
     # Launch threads
     #while urlTab:
     for x in range(0, 3):
-        t = threading.Thread(target=getSparqlFromUrlThreaded, args=(urlTab[int(x*size/4):int((x+1)*size/4)], urlDict, requestType))
+        t = threading.Thread(target=getSparqlFromUrlThreaded, args=(urlTab[int(x*size/4):int((x+1)*size/4)], urlDict, targetDict, requestType, target))
         t.start()
         threads.append(t)
     # Wait for threads
@@ -83,14 +95,18 @@ def getSparqlFromUrls(listOfListsOfUrls, requestType):
                 url = listOfListsOfUrls[pageIdx][urlIdx]
                 if urlDict[url]:#Delete None objects
                     out_list[pageIdx].append(urlDict[url])
+    print(targetDict)
 
     return out_list
 
 
 # Example calls TEST
 # res = getSparqlFromUrl('http://dbpedia.org/resource/Beer', 1)
-# results = getSparqlFromUrls(
-#   [['http://dbpedia.org/resource/Beer', 'http://dbpedia.org/resource/Germany', 'http://dbpedia.org/resource/Europe'],
-#    ['http://dbpedia.org/resource/France', 'http://dbpedia.org/resource/Baguette',
-#     'http://dbpedia.org/resource/Europe']], 0)
+# redirige l'output sur le fichier
+sys.stdout = open('console.txt', 'w', encoding='utf-8')
+results = getSparqlFromUrls(
+  [['http://dbpedia.org/resource/Brad_Pitt'],
+   ['http://dbpedia.org/resource/France'],
+   ['http://dbpedia.org/resource/Angelina_Jolie'],
+   ['http://dbpedia.org/resource/Brad_Davis_(actor)']], 0, 0)
 # print(results)
