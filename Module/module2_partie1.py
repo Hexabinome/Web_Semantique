f3cb9f99f728b497ee5bba104817ef9400a81661
David-Wobrock <david.wobrock@gmail.com>
2015-11-24 09:56:56 +0100

    cache 2-1 +/- ok

--- Module/module2_partie1.py
+++ Module/module2_partie1.py
@@ -3,13 +3,13 @@
 # ------------------------------------------
 import requests, json, threading, os
 
-CACHE_DIRECTORY = 'cache'
+CACHE_DIRECTORY = 'cache/spotlight'
 
 
 # TODO: making the confidence and support changeable
 def getUrlsFromText(url, text, confidence, support):
     cache_file = '{0}/{1}_{2}_{3}.spotlight.txt'.format(CACHE_DIRECTORY,
-                                                        url.replace('http://', '').replace('/', '_').replace(':', '_'),
+                                                        url.replace('http://', '').replace('/', '_').replace(':', '_').replace('?', '_'),
                                                         confidence, support)
     # Try finding URIs in cache
     if os.path.isfile(cache_file):
@@ -17,32 +17,31 @@ def getUrlsFromText(url, text, confidence, support):
         with open(cache_file, 'r') as f:
             try:
                 urlList = f.read().split('\n')
-                # if len(urlList) > 0:
-                #    print("Got uri of {0} from cache".format(cache_file))
+                #print("Loaded from {0} from cache".format(cache_file))
             except:
                 pass
         # If the cache_content is still false, remove existing invalid cache file + send request
         if len(urlList) == 0:
             os.remove(cache_file)
+            #print("Error loading cache {0}".format(cache_file))
             return getUrlsFromText(url, text, confidence, support)
     else:
         data = {
             'text': text,
             'confidence': confidence,
             'support': support}
-        url = 'http://spotlight.dbpedia.org/rest/annotate/'
+        urlSpotlight = 'http://spotlight.dbpedia.org/rest/annotate/'
         header = {
             'Accept': 'application/json',
             'User-Agent':
                 'Mozilla/5.0 (X11; Linux i686; rv:5.0) Gecko/20100101 Firefox/5.0'}
 
-        req = requests.post(url, data, headers=header)
+        req = requests.post(urlSpotlight, data, headers=header)
 
         # TODO can throw error. Check status code 200
         if req.status_code != 200:
             raise IOError("ERR : {0}({1})\nJson was : {2}".format(req.reason, req.status_code, text))
         jsonResponse = json.loads(req.text)
-        # print(jsonResponse.keys())
         urlList = []
         for resource in jsonResponse[u'Resources']:
             urlList.append(resource[u'@URI'])
@@ -53,7 +52,7 @@ def getUrlsFromText(url, text, confidence, support):
             with open(cache_file, 'w') as f:
                 f.write('\n'.join(urlList))
         except:
-            # print('Cache writing error (spotlight) {0}'.format(cache_file))
+            #print('Cache writing error (spotlight) {0}'.format(cache_file))
             pass
 
     return urlList
@@ -61,9 +60,10 @@ def getUrlsFromText(url, text, confidence, support):
 
 def getUrlsFromTextThreaded(texts, result):
     for text in texts:
+        if text['text'] != "":
         # TODO : make confidence & support changeable
-        for url in getUrlsFromText(text['url'], text['text'], 0.2, 20):
-            result[text['url']].add(url)
+            for uri in getUrlsFromText(text['url'], text['text'], 0.2, 20):
+                result[text['url']].add(uri)
 
 
 '''
@@ -74,11 +74,14 @@ Return : A dictionnary {url1: [foundUrl, foundUrl, ...], url2: [],...}
 
 
 def getUrlsFromTexts(jsonTexts):
-    nbTexts = len(jsonTexts)
     result = {}
     for dict in jsonTexts:
-        result[dict['url']] = set()
+        if not dict['text']:
+            jsonTexts.remove(dict) # Remove where text is empty
+        else:
+            result[dict['url']] = set()
     threads = []
+    nbTexts = len(jsonTexts)
     # Launch threads
     nbThreads = min(5, nbTexts)
     for i in range(nbThreads):
